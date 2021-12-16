48482a641e320505fbace0aed87c647ae6bd91a9
Nwosu-Ihueze <nwosunneomagmail.com>
2021-12-15 20:18:36 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+/**
+ * https://vitejs.dev/config/
+ * @type { import('vite').UserConfig }
+ */
+export default {
+  define: {
+    global: {}
+  },
+  plugins: [reactRefresh()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      port: 443,
+    }
+  }
+}
